3b8d3c26a6a660179f691f29857e2e018b41a297
minsu <>
2025-11-11 21:23:07 +0900

    mod: 디테일 페이지 내부에서 api 호출하도록 변경

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -1,5 +1,44 @@
-import { ProductDetail } from "../components/ProductDetail";
+import { getProduct } from "../api/productApi.js";
+import { ProductDetail } from "../components/ProductDetail.js";
+import { router } from "../router/index.js";
 
-export const DetailPage = ({ loading, product }) => {
-  return ProductDetail({ loading, product });
+// 상태 관리 객체
+const state = {
+  product: null,
+  loading: true,
+  productId: null,
+
+  update(newState) {
+    console.log("update", newState);
+    Object.assign(this, newState);
+    router.rerender();
+  },
+
+  async fetchProduct() {
+    this.loading = true;
+    router.rerender();
+
+    try {
+      const data = await getProduct(this.productId);
+      console.log(data);
+      this.update({
+        product: data,
+        loading: false,
+      });
+    } catch (error) {
+      console.error(error);
+      this.update({ loading: false });
+    }
+  },
+};
+
+export const DetailPage = (params) => {
+  const productId = params.id;
+
+  if (state.productId !== productId) {
+    state.productId = productId;
+    state.fetchProduct();
+  }
+
+  return ProductDetail({ loading: state.loading, product: state.product });
 };
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,16 +1,13 @@
-import { getProduct } from "../api/productApi.js";
 import { DetailPage } from "../pages/DetailPAge.js";
 import { HomePage } from "../pages/Homepage.js";
 
 export const setupRoutes = (router) => {
-  router
-    .addRoute("/", async () => {
-      return HomePage();
-    })
-    .addRoute("/products/:id", async (params) => {
-      const data = await getProduct(params.id);
-      return DetailPage({ loading: false, product: data });
-    });
+  router.addRoute("/", async () => {
+    return HomePage();
+  });
+  router.addRoute("/products/:id", async (params) => {
+    return DetailPage(params);
+  });
 
   return router;
 };
