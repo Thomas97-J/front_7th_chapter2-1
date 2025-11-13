bd2b4bb93deefc5b7f14a6ad8633eb657720f026
minsu <>
2025-11-13 23:02:26 +0900

    add: 배포용 브랜치 생성

--- src/router/index.js
+++
@@ -1,8 +0,0 @@
-import { createRouter } from "./router.js";
-import { setupRoutes } from "./routes.js";
-
-// 라우터 생성 및 라우트 설정
-const router = createRouter();
-setupRoutes(router);
-
-export { router };
